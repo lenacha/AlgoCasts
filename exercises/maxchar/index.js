a58562a4f9b4a9caac660ad8b13f5dbb590d6caa
Yerin Cha <yerincha@Yerins-MacBook-Pro.local>
2019-07-18 12:16:07 -0700

    Implement maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let obj = {};
+    let max = 0;
+    let character = '';
+    for(let char of str) {
+        if(char in obj) {
+            obj[char] += 1;
+            if(max < obj[char]) {
+                max = obj[char];
+                character = char;
+            }
+        } else {
+            obj[char] = 1;
+            if(max === 0) {
+                max = 1;
+                character = char;
+            }
+        }
+    }
+    return character;
+}
 
 module.exports = maxChar;
