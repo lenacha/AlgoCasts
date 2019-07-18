65bdb3a147b9cbe9833e9a3ebd3a198b1b0c3b02
Yerin Cha <yerincha@Yerins-MacBook-Pro.local>
2019-07-18 16:48:50 -0700

    Imeplement anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,49 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    stringA = stringA.toLowerCase().split(' ').join('');
+    stringB = stringB.toLowerCase().split(' ').join('');
+    let helper = (str) => {
+        let obj = {};
+        for(let char of str) {
+            if(char in obj) {
+                obj[char] ++
+            } else {
+                obj[char] = 1
+            }
+        }
+        return obj;
+    }
+    objA = helper(stringA);
+    objB = helper(stringB);
+
+    // let objToArr = (object) => {
+    //     let arr = [];
+    //     for(let key in object) {
+    //         arr.push([key, object[key]]);
+    //     }
+    //     arr.sort((a, b) => {
+    //         if(a[0] > b[0]) {
+    //             return 1;
+    //         } else {
+    //             return -1;
+    //         }
+    //     });
+    //     return arr;
+    // }
+    // return JSON.stringify(objToArr(objA)) === JSON.stringify(objToArr(objB));
+    let cb = (a, b) => {
+        if(a[0] > b[0]) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+    let arrA = Object.keys(objA).sort(cb);
+    let arrB = Object.keys(objB).sort(cb);
+
+    return JSON.stringify(arrA) === JSON.stringify(arrB);
+}
 
 module.exports = anagrams;
