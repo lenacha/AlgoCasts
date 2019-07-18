942ca7fb3ca8e0a7a2345d178971e81c8838a862
Yerin Cha <yerincha@Yerins-MacBook-Pro.local>
2019-07-18 11:49:28 -0700

    Implement reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,35 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// function reverseInt(n) {
+//     let isNegative = false;
+//     if(n < 0) {
+//         isNegative = true;
+//         n = Math.abs(n);
+//     }
+//     let reversed = 0;
+
+//     let helper = (num) => {
+//         if(num === 0) {
+//             return;
+//         } else {
+//             reversed = (reversed * 10) + (num % 10);
+//             helper(Math.floor(num/10));
+//         }
+//     }
+//     helper(n);
+
+//     if(isNegative) {
+//         return reversed * -1;
+//     } else {
+//         return reversed;
+//     }
+// }
+
+
+function reverseInt(n) {
+    const reversed = parseInt(n.toString().split('').reverse().join(''));
+    return reversed * Math.sign(n);
+}
 
 module.exports = reverseInt;
