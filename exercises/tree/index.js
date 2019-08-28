ca56df031529c2df7bf02d919a6cf009d5ce9d35
Yerin Cha <yerincha@yerins-mbp.attlocal.net>
2019-08-28 15:38:22 -0700

    Implement tree constructor

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,46 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+    constructor(val){
+        this.data = val;
+        this.children = [];
+    };
+    add(value){
+        let newNode = new Node(value);
+        this.children.push(newNode);
+    };
+    remove(data){
+        this.children = this.children.filter(node => node.data !== data);
+    }
+}
 
-class Tree {}
+class Tree {
+    constructor(){
+        this.root = null;
+    };
+    traverseBF(fn) {
+        const arr = [this.root];
+        while(arr.length > 0) {
+           let node = arr.shift();
+           node.children.forEach(nd => arr.push(nd));
+           fn(node);
+        }
+    };
+
+    traverseDF(fn) {
+        let helper = (node) => {
+            if(!node) {
+                return;
+            } else {
+                fn(node);
+                for(let nd of node.children) {
+                    helper(nd);
+                }
+            }
+        }
+        helper(this.root);
+    };
+}
 
 module.exports = { Tree, Node };
